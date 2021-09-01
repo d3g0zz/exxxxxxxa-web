4591d405524ab18e3613d7806556fd566e576438
d3g0zz <h1d3g0zz@gmail.com>
2021-08-31 22:41:08 -0500

    add Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { FaBars } from "react-icons/fa"
+import { menuData } from "../data/MenuData"
+import { Button } from "./Button"
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>
+                        Exxxxxxxa
+                    </NavLogo>
+                </NavbarContainer>
+
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
